d6e0262b91c20e3a7b57fd443beec2babeeb9643
t.kurtovic <tkurtovic98@gmail.com>
2022-05-21 19:51:03 +0200

    Add retrieval of employees and items.

---
+++ Client/app/src/api/models/ApiProduct.tsx
@@ -0,0 +1,7 @@
+export type ApiProduct = {
+  ponudaid: number;
+  naziv: string;
+  jedcijena: number;
+  kolicinadostupno: number;
+  kolicinakriticno: number;
+};
--- Client/app/src/components/forms/OrderForm.tsx
+++ Client/app/src/components/forms/OrderForm.tsx
@@ -1,65 +1,38 @@
-import { AddIcon, MinusIcon, PlusSquareIcon } from "@chakra-ui/icons";
-import {
-  FormLabel,
-  FormControl,
-  Input,
-  Button,
-  Box,
-  Select,
-  Heading,
-  Stack,
-  Flex,
-  OrderedList,
-  UnorderedList,
-  ListItem,
-  IconButton,
-  Text,
-} from "@chakra-ui/react";
-import React, { FunctionComponent, useState } from "react";
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
+import * as react from "@chakra-ui/react";
+import { FunctionComponent, useEffect, useState } from "react";
+import { AvailableItem } from "../../models/AvailableItem";
+import { Employee } from "../../models/Employee";
+import { fetchEmployees } from "../../source/employee/EmployeeSource";
+import { fetchItems } from "../../source/items/ItemsSource";
 
-type Item = {
-  id: number;
-  quantity: number;
-};
-
-type Employee = {
-  id: number;
-  name: string;
-};
-
-const mockEmployees: Array<Employee> = [
-  { id: 1, name: "Fran" },
-  { id: 2, name: "Joža" },
-  { id: 3, name: "Pero" },
-  { id: 4, name: "lala" },
-];
-
-type AvailableItem = {
-  id: number;
-  name: string;
-};
-
-const mockAvailableItems = new Map<number, AvailableItem>([
-  [1, { id: 1, name: "Nalivpero" }],
-  [2, { id: 2, name: "Bilježnica" }],
-  [3, { id: 3, name: "Uvez rada" }],
-  [4, { id: 4, name: "Knjiga" }],
-  [5, { id: 5, name: "Žvake" }],
-]);
 export const OrderForm: FunctionComponent = () => {
-  const [employees, setEmployees] = useState<Array<Employee>>(mockEmployees);
-  const [availableItems, setAvailableItems] =
-    useState<Map<number, AvailableItem>>(mockAvailableItems);
+  const [employees, setEmployees] = useState<Array<Employee>>([]);
+  const [availableItems, setAvailableItems] = useState<
+    Map<number, AvailableItem>
+  >(new Map<number, AvailableItem>());
 
   const [selectedEmployee, setSelectedEmployee] = useState<Employee>();
   const [email, setEmail] = useState("");
   const [selectedItems, setSelectedItems] = useState<Map<number, number>>(
-    new Map<number, number>(
-      Array.from(availableItems).map(([id, _]) => [id, 0])
-    )
+    new Map<number, number>()
   );
 
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    fetchEmployees().then((data) => setEmployees(data));
+    fetchItems().then((data) => {
+      setAvailableItems(
+        new Map<number, AvailableItem>(
+          Array.from(data).map((item) => [item.id, item])
+        )
+      );
+      setSelectedItems(
+        new Map<number, number>(Array.from(data).map((item) => [item.id, 0]))
+      );
+    });
+  }, []);
 
   const handleError = (message: string) => {
     console.log(message);
@@ -80,8 +53,6 @@ export const OrderForm: FunctionComponent = () => {
       /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
     );
 
-    console.log(`EMAIL ${email}`);
-
     if (!pattern.test(email)) {
       handleError("Neispravan e-mail!");
       return;
@@ -95,7 +66,7 @@ export const OrderForm: FunctionComponent = () => {
     console.log(
       `Email: ${email}, Djelatnik: ${JSON.stringify(
         selectedEmployee
-      )}, Proizvodi: ${JSON.stringify([...selectedItems])}`
+      )}, Proizvodi: ${JSON.stringify([...selectedItems!])}`
     );
   };
 
@@ -108,108 +79,112 @@ export const OrderForm: FunctionComponent = () => {
 
     currentSelectedItems.set(id, currentSelectedItems.get(id)! + changeBy);
 
-    console.log(currentSelectedItems);
-
     setSelectedItems(currentSelectedItems);
   };
 
   return (
-    <Box my={4} p={16} textAlign="left">
+    <react.Box my={4} p={16} textAlign="left">
       <form
         onSubmit={(event) => {
           event.preventDefault();
           handleSubmit();
         }}
       >
-        <Stack direction={{ base: "column", md: "row" }} spacing={8}>
-          <Flex flex={1} direction={"column"}>
-            <FormControl isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input
+        <react.Stack direction={{ base: "column", md: "row" }} spacing={8}>
+          <react.Flex flex={1} direction={"column"}>
+            <react.FormControl isRequired>
+              <react.FormLabel>Email</react.FormLabel>
+              <react.Input
                 onChange={(event) => setEmail(event.target.value)}
                 type="email"
                 placeholder="kupac@skrpt.hr"
               />
-            </FormControl>
-            <FormControl mt={6} isRequired>
-              <FormLabel>Djelatnik</FormLabel>
-              <Select
-                onChange={(event) =>
-                  handleEmployeeChange(JSON.parse(event.target.value))
-                }
-              >
-                {employees.map((employee) => {
-                  return (
-                    <option value={JSON.stringify(employee)}>
-                      {employee.name}
-                    </option>
-                  );
-                })}
-              </Select>
-            </FormControl>
-            <FormControl mt={6} isRequired>
-              <FormLabel>Proizvod</FormLabel>
-              <Select
-                onChange={(event) =>
-                  updateItemQuantity(parseInt(event.target.value), 1)
-                }
-              >
-                {Array.from(availableItems).map(([id, item]) => {
-                  return <option value={id}>{item.name}</option>;
-                })}
-              </Select>
-            </FormControl>
-          </Flex>
-          <Flex flex={1}>
-            <UnorderedList>
-              {Array.from(selectedItems)
-                .filter(([id, quantity]) => {
-                  return quantity > 0;
-                })
-                .map(([id, quantity]) => {
-                  return (
-                    <Box
-                      scrollBehavior={"smooth"}
-                      display={"flex"}
-                      flexDirection={"row"}
-                      alignItems={"center"}
-                      p={8}
-                    >
-                      <IconButton
-                        icon={<MinusIcon />}
-                        aria-label="Minus"
-                        onClick={(event) => {
-                          if (quantity > 0) updateItemQuantity(id, -1);
-                        }}
-                      />
-                      <Text
-                      w={"full"}
-                        mx={4}
-                        fontSize={{ base: "lg", md: "2xl" }}
-                        textAlign={"center"}
+            </react.FormControl>
+            {employees.length > 0 && (
+              <react.FormControl mt={6} isRequired>
+                <react.FormLabel>Djelatnik</react.FormLabel>
+                <react.Select
+                  onChange={(event) =>
+                    handleEmployeeChange(JSON.parse(event.target.value))
+                  }
+                >
+                  {employees.map((employee) => {
+                    return (
+                      <option value={JSON.stringify(employee)}>
+                        {employee.name} {employee.lastName}
+                      </option>
+                    );
+                  })}
+                </react.Select>
+              </react.FormControl>
+            )}
+
+            {availableItems.size > 0 && (
+              <react.FormControl mt={6} isRequired>
+                <react.FormLabel>Proizvod</react.FormLabel>
+                <react.Select
+                  onChange={(event) =>
+                    updateItemQuantity(parseInt(event.target.value), 1)
+                  }
+                >
+                  {Array.from(availableItems!).map(([id, item]) => {
+                    return <option value={id}>{item.name}</option>;
+                  })}
+                </react.Select>
+              </react.FormControl>
+            )}
+          </react.Flex>
+          <react.Flex flex={1} align={"center"} justify={"center"}>
+            {selectedItems.size > 0 && (
+              <react.UnorderedList>
+                {Array.from(selectedItems!)
+                  .filter(([id, quantity]) => {
+                    return quantity > 0;
+                  })
+                  .map(([id, quantity]) => {
+                    return (
+                      <react.Box
+                        as={react.Flex}
+                        align={"center"}
+                        justify={"center"}
+                        p={{ base: 2, md: 8 }}
                       >
-                        {availableItems.get(id)?.name}: {quantity}
-                      </Text>
-                      <IconButton
-                        icon={<AddIcon />}
-                        aria-label="Plus"
-                        onClick={(event) => {
-                          updateItemQuantity(id, 1);
-                        }}
-                      />
-                    </Box>
-                  );
-                })}
-            </UnorderedList>
-          </Flex>
-        </Stack>
+                        <react.IconButton
+                          icon={<MinusIcon />}
+                          aria-label="Minus"
+                          onClick={(event) => {
+                            if (quantity > 0) updateItemQuantity(id, -1);
+                          }}
+                        />
+                        <react.Text
+                          w={"full"}
+                          mx={{base: 2, md: 4}}
+                          fontSize={{ base: "lg", md: "2xl" }}
+                          textAlign={"center"}
+                        >
+                          {availableItems!.get(id)?.name}: {quantity}
+                        </react.Text>
+                        <react.IconButton
+                          icon={<AddIcon />}
+                          aria-label="Plus"
+                          onClick={(event) => {
+                            updateItemQuantity(id, 1);
+                          }}
+                        />
+                      </react.Box>
+                    );
+                  })}
+              </react.UnorderedList>
+            )}
+          </react.Flex>
+        </react.Stack>
         {errorMessage.length > 0 && (
-          <Heading color={"red.600"}>{errorMessage}</Heading>
+          <react.Heading color={"red.600"}>{errorMessage}</react.Heading>
         )}
-        <Button width="full" mt={4} type="submit">
+        <react.Button width="full" mt={4} type="submit">
           Dodaj narudžbu
-        </Button>
+        </react.Button>
       </form>
-    </Box>
+    </react.Box>
   );
 };
