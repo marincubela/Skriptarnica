b8532b96a5232211733deebbe9ed144d877cb5e2
t.kurtovic <tkurtovic98@gmail.com>
2022-05-21 19:54:07 +0200

    Add server cors configuration.

--- Server/src/main/java/hr/unizg/fer/infsus/skriptarnica/SkriptarnicaApplication.java
+++ Server/src/main/java/hr/unizg/fer/infsus/skriptarnica/SkriptarnicaApplication.java
@@ -2,6 +2,11 @@ package hr.unizg.fer.infsus.skriptarnica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SkriptarnicaApplication {
@@ -11,3 +16,4 @@ public class SkriptarnicaApplication {
 	}
 
 }
+
--- Server/src/main/java/hr/unizg/fer/infsus/skriptarnica/repository/ProizvodRepository.java
+++ Server/src/main/java/hr/unizg/fer/infsus/skriptarnica/repository/ProizvodRepository.java
@@ -4,6 +4,9 @@ import hr.unizg.fer.infsus.skriptarnica.model.Proizvod;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ProizvodRepository extends CrudRepository<Proizvod, Long> {
+    Iterable<Proizvod> findProizvodsByPonudaidIn(Collection<Long> ponudaid);
 }
